12073eb9623f3d925f0b009e66cb593ba8b2ffab
cjam27 <christianflynn05@gmail.com>
2026-01-21 13:00:52 -0500

    Fix for scanner, moved some if else statements.

--- CLI Port Scanner/Scanner.py
+++ CLI Port Scanner/Scanner.py
@@ -6,34 +6,27 @@ target = input("Enter the host to scan: ")
 
 try:
     ip = socket.gethostbyname(target)
-    print(f"Target IP: {ip}")
+
+    port_number = int(input("Enter port number: "))
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    s.settimeout(1)
+
+    result = s.connect_ex((ip, port_number))
+    if result == 0:
+        print("Port is open")
+    else:
+        print("Port is closed")
+
+    try:
+        s.shutdown(socket.SHUT_WR)
+    except OSError as e:
+        print(f"Error during shutdown: {e}")
+    finally:
+        s.close()
+
 except socket.gaierror:
     print("Could not resolve hostname.")
 
-# input target IP
-# input port number
-# create TCP socket
-# set timeout
-# attempt connect_ex
-# if result == 0 → print open
-# else → print closed
-# close socket
-ip = input("Enter the host ip: ")
-port_number = int(input("Enter port number: "))
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-s.settimeout(1)
-
-result = s.connect_ex((ip, port_number))
-if result == 0:
-    print("Port is open")
-else:
-    print("Port is closed")
-
-try:
-    s.shutdown(socket.SHUT_WR)
-except OSError as e:
-    print(f"Error during shutdown: {e}")
-finally:
-    s.close()
