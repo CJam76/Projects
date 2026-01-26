d039717d2d7a22ebd9469dea38f02665add6d732
cjam27 <christianflynn05@gmail.com>
2026-01-26 11:56:45 -0500

    Reorganized some code.

--- CLI Port Scanner/Scanner.py
+++ CLI Port Scanner/Scanner.py
@@ -14,6 +14,15 @@ try:
     s.settimeout(1)
 
     result = s.connect_ex((ip, port_number))
+    # for port in range(start, end + 1):
+    #     check
+    #     port
+    #     using
+    #     your
+    #     existing
+    #     logic
+    #     print
+    #     result if open
     if result == 0:
         print("Port is open")
     else:
